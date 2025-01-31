646fa4c9a9ebd335a533718f253d05b5d9f8cb6f
giacomo-bearzi <giacomo.bearzi@hotmail.it>
2025-01-31 10:01:25 +0100

    Add CountriesAndFlagsApi to fetch country data from REST API

---
+++ countries_and_flags/lib/api/countries_and_flags_api.dart
@@ -0,0 +1,25 @@
+import 'package:countries_and_flags/api/http_client_provider.dart';
+import 'package:countries_and_flags/api/models/rest_countries_response_api_model.dart';
+import 'package:dio/dio.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+part 'countries_and_flags_api.g.dart';
+
+@riverpod
+CountriesAndFlagsApi countriesAndFlagsApi(CountriesAndFlagsApiRef ref) {
+  final client = ref.watch(httpClientProvider);
+  return CountriesAndFlagsApi(client);
+}
+
+class CountriesAndFlagsApi {
+  const CountriesAndFlagsApi(this.client);
+  final Dio client;
+
+  Future<List<RestCountriesResponseApiModel>> fetchAll() async {
+    final response = await client.get('/v3.1/all');
+    final model = List.from(response.data);
+    return model
+        .map((countryJson) =>
+            RestCountriesResponseApiModel.fromJson(countryJson))
+        .toList();
+  }
+}
---
+++ countries_and_flags/lib/api/countries_and_flags_api.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'countries_and_flags_api.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$countriesAndFlagsApiHash() =>
+    r'16cdd2435cbbd1fadac43ce66909646fa343546e';
+
+/// See also [countriesAndFlagsApi].
+@ProviderFor(countriesAndFlagsApi)
+final countriesAndFlagsApiProvider =
+    AutoDisposeProvider<CountriesAndFlagsApi>.internal(
+  countriesAndFlagsApi,
+  name: r'countriesAndFlagsApiProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$countriesAndFlagsApiHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef CountriesAndFlagsApiRef = AutoDisposeProviderRef<CountriesAndFlagsApi>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
