fbfb09317fbf0349c8699ece0e394813da7be010
giacomo-bearzi <giacomo.bearzi@hotmail.it>
2025-01-31 11:20:17 +0100

    Add route for FavoritesPage to the router

--- countries_and_flags/lib/router.dart
+++ countries_and_flags/lib/router.dart
@@ -1,5 +1,6 @@
 import 'package:countries_and_flags/src/models/rest_country_model.dart';
 import 'package:countries_and_flags/src/pages/details_page.dart';
+import 'package:countries_and_flags/src/pages/favorites_page.dart';
 import 'package:countries_and_flags/src/pages/home_page.dart';
 import 'package:go_router/go_router.dart';
 import 'logger.dart';
@@ -22,6 +23,11 @@ final router = GoRouter(
         final country = state.extra as RestCountryModel;
         return DetailsPage(country);
       },
+    ),
+    GoRoute(
+      path: '/favorites',
+      name: 'favorites',
+      builder: (context, state) => const FavoritesPage(),
     )
   ],
 );
